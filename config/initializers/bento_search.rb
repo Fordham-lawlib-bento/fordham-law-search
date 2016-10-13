5ce1102aa288eaeaaab181da235fe60c69aec3d5
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-10-13 16:00:34 -0400

    lame placeholder css

--- config/initializers/bento_search.rb
+++ config/initializers/bento_search.rb
@@ -1,24 +1,30 @@
 BentoSearch.register_engine("catalog") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 6
 end
 
 BentoSearch.register_engine("articles") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 8
 end
 
 BentoSearch.register_engine("reserves") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 3
 end
 
 BentoSearch.register_engine("databases") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 5
 end
 
 BentoSearch.register_engine("website") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 3
 end
 
 # bepress institutional repository
 BentoSearch.register_engine("flash") do |conf|
   conf.engine = "BentoSearch::MockEngine"
+  conf.num_results = 2
 end
