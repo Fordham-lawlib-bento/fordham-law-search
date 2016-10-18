59dce3406cd9f0b81b42e2327bad1b6896ab9469
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-10-18 11:13:36 -0400

    title and heading from config, total hits

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Developed by Friends of the Web http://friendsoftheweb.com
 
 This app was generated with rails new ... --skip-activerecord --skip-spring --skip-turbolinks
 We don't need a database for this first version of the app.
+
+## Configuration
+
+Configuration of search targets and text related to each is in ./configuration/bento_search.rb
--- config/initializers/bento_search.rb
+++ config/initializers/bento_search.rb
@@ -1,30 +1,58 @@
 BentoSearch.register_engine("catalog") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 6
+
+  conf.for_display do |display|
+    display.heading = "Catalog"
+    display.hint = "Library books, journals, music, videos, databases, archival collections, and online resources"
+  end
 end
 
 BentoSearch.register_engine("articles") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 8
+
+  conf.for_display do |display|
+    display.heading = "Articles"
+    display.hint = "Articles, e-books, dissertations, music, images, and more from a mostly full-text database"
+  end
 end
 
 BentoSearch.register_engine("reserves") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 3
+
+  conf.for_display do |display|
+    display.heading = "Reserves/Exams"
+  end
 end
 
 BentoSearch.register_engine("databases") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 5
+
+  conf.for_display do |display|
+    display.heading = "Databases"
+  end
 end
 
 BentoSearch.register_engine("website") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 3
+
+  conf.for_display do |display|
+    display.heading = "Library Website"
+    display.hint = "Information about the libraries from the The Maloney Library website"
+  end
 end
 
 # bepress institutional repository
 BentoSearch.register_engine("flash") do |conf|
   conf.engine = "BentoSearch::MockEngine"
   conf.num_results = 2
+
+  conf.for_display do |display|
+    display.heading = "FLASH"
+    display.hint = "Fordham Law Archive of Scholarship & History"
+  end
 end
