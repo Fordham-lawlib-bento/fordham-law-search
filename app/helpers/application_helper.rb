c22b30994dae9890a7c47d6ea7ae0e4fd7c96eda
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-11-15 10:54:38 -0500

    search type as radio button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def radio_button(name:, value:, label:)
+    label_tag do
+      radio_button_tag(name, value.to_s, params[name].to_s == value.to_s) +
+        label
+    end
+  end
 end
