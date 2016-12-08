29e7539deb4ac33e12e8efd14fb75da291239292
Jonathan Rochkind <jonathan@friendsoftheweb.com>
2016-12-08 13:57:13 -0500

    de-thorn rack timeout in development

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,2 @@
-Rack::Timeout.timeout = 20  # seconds
+# Effectively no timeout in development please, so we can debug and take our time
+Rack::Timeout.timeout = Rails.env.development? ? 10000 : 20  # seconds
